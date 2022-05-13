423348149a61aea1de38cc70d81b509887714842
sharmadotankit <sharma.ankit.kamal@gmail.com>
2022-05-13 09:12:19 +0530

    adding connectionString

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ app.use(express.json());
 const db = knex({
   client: 'pg',
   connection: {
-	  host : process.env.DATABASE_URL,
+	  connectionString : process.env.DATABASE_URL,
     ssl:true,
   }
 });
